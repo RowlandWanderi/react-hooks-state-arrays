16a092659e6ca47f38dccbbadfe8936e0d60d0e4
Rowland <rowland.wanderi@student.moringaschool.com>
2023-10-26 23:12:17 +0300

    complete assignment

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -1,23 +1,64 @@
 import React, { useState } from "react";
 import { spicyFoods, getNewRandomSpicyFood } from "../data";
 
+
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
+  const [filterBy, setFilterBy] = useState("All");
+  console.log(foods)
+  const foodsToDisplay = foods.filter(food => {
+    if (filterBy === "All") {
+      return true;
+    } else {
+      return food.cuisine === filterBy;
+    }
+  });
+  const handleFilterChange = e => {
+    setFilterBy(e.target.value);
+  };
 
-  function handleAddFood() {
+  const addNewFood = () => {
     const newFood = getNewRandomSpicyFood();
-    console.log(newFood);
-  }
+    const newSpicyFood = [...foods, newFood];
+    setFoods(newSpicyFood);
+  };
+  const removeFood = id => {
+    setFoods(foods.filter(food => food.id !== id));
+  };
 
-  const foodList = foods.map((food) => (
-    <li key={food.id}>
-      {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
-    </li>
-  ));
+  const updateFood = id => {
+    setFoods(
+      foods.map(food => {
+        if (food.id === id) {
+          return { ...food, heatLevel: food.heatLevel + 1 };
+        } else {
+          return food;
+        }
+      })
+    );
+  };
+
+  const foodList = foodsToDisplay.map(food => {
+    return (
+      <React.Fragment>
+        <li key={food.id} onClick={() => updateFood(food.id)}>
+          {food.name}| Heat: {food.heatLevel} | Cuisine: {food.cuisine}{" "}
+        </li>
+        <button onClick={() => removeFood(food.id)}>Remove</button>
+      </React.Fragment>
+    );
+  });
 
   return (
     <div>
-      <button onClick={handleAddFood}>Add New Food</button>
+      <button onClick={addNewFood}>Add Spicy Food</button>
+      <select name="filter" onChange={handleFilterChange}>
+        <option value="All">All</option>
+        <option value="American">American</option>
+        <option value="Sichuan">Sichuan</option>
+        <option value="Thai">Thai</option>
+        <option value="Mexican">Mexican</option>
+      </select>
       <ul>{foodList}</ul>
     </div>
   );
